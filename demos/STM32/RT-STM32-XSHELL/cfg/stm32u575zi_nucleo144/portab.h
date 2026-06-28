18fc53f4f5e2137b7ab3f792860311a67e15e14c
Giovanni Di Sirio <gdisirio@gmail.com>
2026-06-28 07:40:42 +0200

    Fix U5 XSHELL portability header style

--- demos/STM32/RT-STM32-XSHELL/cfg/stm32u575zi_nucleo144/portab.h
+++ demos/STM32/RT-STM32-XSHELL/cfg/stm32u575zi_nucleo144/portab.h
@@ -30,7 +30,6 @@
 /*===========================================================================*/
 
 #define PORTAB_LINE_LED1            LINE_LED_GREEN
-//#define PORTAB_LINE_LED2            LINE_LED2
 #define PORTAB_LED_OFF              PAL_LOW
 #define PORTAB_LED_ON               PAL_HIGH
 
